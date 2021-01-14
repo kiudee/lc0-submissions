9c630795a53a836ec27f61b4e3de17a918f58f79
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-01-14 21:14:07 +0100

    Make update script robust to branch deletions

--- gpu/update.sh
+++ gpu/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-git clone --recurse-submodules https://github.com/Tilps/lc0.git
+git clone --recurse-submodules https://github.com/kiudee/lc0.git
 cd lc0
-git checkout mgp_spinlocks_refactored
+git checkout pr1483_sufi
 read -p "Download done, press enter to continue"
 CC=clang-6.0 CXX=clang++-6.0 ./build.sh
 EXE=${PWD}/build/release/lc0
