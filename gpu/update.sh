43418fdd28483253146f5035f5d5bbf3a3dcc179
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-02-20 12:17:42 +0100

    Switch to updated multigather_only branch

--- gpu/update.sh
+++ gpu/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-git clone --recurse-submodules https://github.com/kiudee/lc0.git
+git clone --recurse-submodules https://github.com/Tilps/lc0.git
 cd lc0
-git checkout sufibonus2
+git checkout multigather_only
 read -p "Download done, press enter to continue"
 CC=clang-6.0 CXX=clang++-6.0 ./build.sh release -Dmalloc=tcmalloc
 EXE=${PWD}/build/release/lc0
