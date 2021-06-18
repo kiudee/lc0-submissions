a5ea80776725876fccc5930150313257cba82bcf
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-06-18 17:41:59 +0200

    Use the latest v0.28.0-rc1

--- tcec/gpu/update.sh
+++ tcec/gpu/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-git clone --recurse-submodules https://github.com/Tilps/lc0.git
+git clone --recurse-submodules git clone --recurse-submodules https://github.com/LeelaChessZero/lc0.git
 cd lc0
-git checkout multigather_only_mcv
+git checkout v0.28.0-rc1
 read -p "Download done, press enter to continue"
 CC=clang-6.0 CXX=clang++-6.0 ./build.sh release -Dmalloc=tcmalloc
 EXE=${PWD}/build/release/lc0
