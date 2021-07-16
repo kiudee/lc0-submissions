1255f3d8a08aee2a6a49af373397b7ab966cb328
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-07-16 18:10:34 +0200

    Change to sufi21 branch

--- archive/tcec/S21/Sufi/update.sh
+++ archive/tcec/S21/Sufi/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-git clone --recurse-submodules https://github.com/LeelaChessZero/lc0.git
+git clone --recurse-submodules https://github.com/kiudee/lc0.git
 cd lc0
-git checkout v0.28.0-rc1
+git checkout sufi21
 read -p "Download done, press enter to continue"
 CC=clang-6.0 CXX=clang++-6.0 ./build.sh release -Dmalloc=tcmalloc
 EXE=${PWD}/build/release/lc0
