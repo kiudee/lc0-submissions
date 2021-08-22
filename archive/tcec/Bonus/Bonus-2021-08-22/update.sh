d1977d6a76dffe824ee50fec2a46f033aca4347d
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-08-22 16:29:53 +0200

    Exchange clang v6 for system clang

--- archive/tcec/Bonus/Bonus-2021-08-22/update.sh
+++ archive/tcec/Bonus/Bonus-2021-08-22/update.sh
@@ -3,5 +3,5 @@ git clone --recurse-submodules https://github.com/LeelaChessZero/lc0.git
 cd lc0
 git checkout release/0.28
 read -p "Download done, press enter to continue"
-CC=clang-6.0 CXX=clang++-6.0 ./build.sh release -Dmalloc=tcmalloc
+CC=clang CXX=clang++ ./build.sh release -Dmalloc=tcmalloc
 EXE=${PWD}/build/release/lc0
