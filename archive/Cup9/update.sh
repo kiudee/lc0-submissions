d99a2f57529fd612421f69a51d2bfd5ee74b0aaa
OfekShochat <o@shoch.at>
2021-11-01 11:08:19 +0200

    cup9

--- archive/Cup9/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone --recurse-submodules https://github.com/kiudee/lc0.git
-cd lc0
-git checkout sufi21
-read -p "Download done, press enter to continue"
-CC=clang-6.0 CXX=clang++-6.0 ./build.sh release -Dmalloc=tcmalloc
-EXE=${PWD}/build/release/lc0
